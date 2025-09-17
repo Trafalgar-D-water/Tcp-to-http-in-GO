0f55c1c6f08a3ee7ad2942ff0dfe99324d1e5e66
priyanshu patnayak <patnayakpriyanshu06@gmail.com>
2025-09-17 16:17:36 +0530

    get reader

--- main.go
+++ main.go
@@ -3,38 +3,51 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
+func getLinesChannels(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+		str := ""
+		for {
+			data := make([]byte, 8) // 8 byte slice
+			n, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			str = ""
+			data = data[:n]
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str
+				str = ""
+			}
+
+			str += string(data)
+		}
+
+		if len(str) != 0 {
+			out <- str
+		}
+
+	}()
+	return out
+}
 func main() {
-	// dir, _ := os.Getwd()
-	// fmt.Println("Current working dir:", dir)
 	f, err := os.Open("message.txt")
 	if err != nil {
 		log.Fatal("error", "error", err)
 	}
 
-	str := ""
-	for {
-		data := make([]byte, 8)
-		n, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		str = ""
-		data = data[:n]
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-
-		str += string(data)
+	lines := getLinesChannels(f)
+	for line := range lines {
+		fmt.Printf("read : %s\n", line)
 	}
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
-	}
-
 }
